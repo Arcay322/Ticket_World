091754f8ab3ec0583fe4f1cdcd486e2354db8756
Arcay322 <169007284+Arcay322@users.noreply.github.com>
2025-07-01 01:52:00 -0500

    debug

--- ticket_world/settings.py
+++ ticket_world/settings.py
@@ -311,3 +311,25 @@ INTERNAL_IPS = [
 ]
 # settings.py
 MAPS_API_KEY = os.getenv('MAPS_API_KEY')
+
+# --- CONFIGURACIÓN DE LOGGING PARA DEPURACIÓN EN PRODUCCIÓN ---
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'propagate': False,
+        },
+    },
+}
